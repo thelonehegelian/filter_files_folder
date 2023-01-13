72342cf340beb0b9889355f318456c5f1e90ecf9
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 00:04:58 +0000

    Add contains loop function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use same_file::is_same_file;
+use std::io;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
+
 fn main() {
     // get current directory
     let current_dir = env::current_dir().unwrap();
@@ -8,5 +12,23 @@ fn main() {
         let metadata = fs::metadata(entry).unwrap();
         let last_modified = metadata.modified().unwrap().elapsed().unwrap().as_secs();
         println!("{:?}", last_modified);
+    }
+
+    /************************
+     * DETECT LOOP OF A PATH
+     ************************/
+
+    fn contains_loop<P: AsRef<Path>>(path: P) -> io::Result<Option<(PathBuf, PathBuf)>> {
+        let path = path.as_ref();
+        let mut path_buf = path.to_path_buf();
+
+        while path_buf.pop() {
+            if is_same_file(&path_buf, path).unwrap() {
+                return Ok(Some((path_buf, path.to_path_buf())));
+            } else if let Some(looped_paths) = contains_loop(&path_buf).unwrap() {
+                return Ok(Some(looped_paths));
+            }
+        }
+        return Ok(None);
     }
 }
